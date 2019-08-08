465273a7641e4dfce172b698b5d7ef185dde1d31
Erwan Bousse <erwan.bousse@ls2n.fr>
2019-08-08 14:37:16 +0200

    Update compile script + demo html

--- compile.sh
+++ compile.sh
@@ -10,9 +10,11 @@ then
     git clone -b 3.8.0 --depth 1 https://github.com/hakimel/reveal.js.git
     cd reveal.js
     npm install
-    ln -s "${PWD}"/../css/theme/source/* ./css/theme/source/ # add our theme to reveal.js
 fi
 
+
 cd "${STARTFOLDER}"/reveal.js
+ln -fs "${PWD}"/../css/theme/source/* ./css/theme/source/ # add our theme to reveal.js
+ln -fs "${PWD}"/../lib/font/* ./lib/font/ # add our fonts
 npm run build -- css-themes # build all reveal.js themes, including ours
 cd "${STARTFOLDER}"
