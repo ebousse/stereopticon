55e082ee312ab437a2285c8ee835445eaff7dba7
Erwan Bousse <erwan.bousse@ls2n.fr>
2024-01-14 16:04:43 +0100

    Fix fonts pats in build

--- compile.sh
+++ compile.sh
@@ -27,7 +27,6 @@ fi
 
 cd "${STARTFOLDER}"/reveal.js
 ln -fs "${PWD}"/../src/css/theme/source/* ./css/theme/source/ # add our theme to reveal.js
-ln -fs "${PWD}"/../src/lib/font/* ./dist/theme/fonts          # add our fonts
 npm run build -- css-themes                                   # build all reveal.js themes, including ours
 cd "${STARTFOLDER}"
 mv reveal.js/node_modules .                                   # remove node_modules from reveal (to not overload gitlab)
